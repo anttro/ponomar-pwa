e397591781d9758814d0fa5be6936bfc25d08a7f
Антон Трошин <i@atroshin.ru>
2026-08-02 01:22:35 +0300

    fix: exclude data files from precache to prevent SW install errors

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
-        globPatterns: ['**/*.{js,css,html,woff,json,png}'],
+        globPatterns: ['**/*.{js,css,html,woff,png}'],
+        globIgnores: ['**/data/**'],
         maximumFileSizeToCacheInBytes: 20 * 1024 * 1024,
         runtimeCaching: [
           {
